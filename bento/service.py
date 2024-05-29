e29a764badecd2a95a3f3c577bcff290d938a7c4
jyk0619 <113344368+jyk0619@users.noreply.github.com>
2024-05-30 02:54:08 +0900

    ml서버

---
+++ bento/service.py
@@ -0,0 +1,41 @@
+
+import bentoml
+from PIL.Image import Image
+
+MODEL_ID = "ttthea/fashion-clothing"
+
+sample_prompt = "A full-body shot of a person wearing a denim jacket, with their head out of the frame, on a grey solid color background."
+
+@bentoml.service(
+    traffic={"timeout": 300},
+    workers=1,
+    resources={
+        "gpu": 1,
+        "gpu_type": "nvidia-l4",
+        # You can also specify GPU memory requirement:
+        # "memory": "16Gi",
+    },
+)
+class SD:
+    def __init__(self) -> None:
+        from diffusers import AutoPipelineForText2Image
+        import torch
+
+        self.pipe = AutoPipelineForText2Image.from_pretrained(
+            MODEL_ID,
+            torch_dtype=torch.float16,
+           
+        )
+        self.pipe.to(device="cuda")
+
+    @bentoml.api
+    def txt2img(
+            self,
+            prompt: str = sample_prompt,
+            
+    ) -> Image:
+        image = self.pipe(
+            prompt=prompt,
+           
+        ).images[0]
+        return image
