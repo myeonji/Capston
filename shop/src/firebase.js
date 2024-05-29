6ae8aa9a92864dedc0aea87e82c929d01327002e
myeonji <imhyunjik@naver.com>
2024-05-30 00:23:17 +0900

    firebase 환경변경 수정6
    .env 파일로
    firebaseconfig를 환경변수로 설정하는 작업 필요

--- shop/src/firebase.js
+++ shop/src/firebase.js
@@ -5,17 +5,18 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyA0nTyxXuNk9P9PoWg4kA936fC9ezq3q1g",
+  authDomain: "capstone-16a74.firebaseapp.com",
+  projectId: "capstone-16a74",
+  storageBucket: "capstone-16a74.appspot.com",
+  messagingSenderId: "650956541054",
+  appId: "1:650956541054:web:833e361d6dcd7d11785c2f",
+  measurementId: "G-6Z6L9HMR0R"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 
-export { db }
+export {db}
