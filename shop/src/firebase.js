19ecebf1241f626198d774dc4ca0949426139d6a
myeonji <imhyunjik@naver.com>
2024-05-29 20:44:34 +0900

    firebase 환경설정

---
+++ shop/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0nTyxXuNk9P9PoWg4kA936fC9ezq3q1g",
+  authDomain: "capstone-16a74.firebaseapp.com",
+  projectId: "capstone-16a74",
+  storageBucket: "capstone-16a74.appspot.com",
+  messagingSenderId: "650956541054",
+  appId: "1:650956541054:web:833e361d6dcd7d11785c2f",
+  measurementId: "G-6Z6L9HMR0R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
